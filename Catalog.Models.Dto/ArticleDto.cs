a096a2c524d11500d63114044b6052b747f81250
SrvDev Bernikova <SrvDev.Bernikova@globus.ru>
2024-06-11 12:08:33 +0300

    DTO, API

---
+++ Catalog.Models.Dto/ArticleDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Models.Dto;
+
+public class ArticleDto
+{
+    public Guid ArticleId { get; set; }
+    public string ArticleNumber { get; set; }
+    public DateTime UpdatedAt  { get; set; }
+    
+
+}
---
+++ Catalog.Models.Dto/PriceDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.Models.Dto;
+
+public class PriceDto
+{
+    public Guid PriceId { get; set;}
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Price { get; set; }
+    
+    public Guid ArticleId { get; set; }
+    
+}
